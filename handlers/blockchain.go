6b5514edfef60cea4df85b45927e9eb963014c00
kminhy09 <kminhy09@gmail.com>
2017-12-11 18:03:02 +0900

    POST /registrar

--- handlers/blockchain.go
+++ handlers/blockchain.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -12,6 +14,30 @@ func GetChain(c echo.Context) error {
 
 	// GET 호출
 	resp, err := http.Get("http://52.78.185.234:7050/chain")
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	// 결과 출력
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
+
+	return c.JSON(http.StatusCreated, string(data))
+}
+
+func PostRegistrar(c echo.Context) error {
+
+	jsonData := map[string]string{"enrollId": "admin", "enrollSecret": "Xurw3yU9zI0l"}
+	jsonValue, _ := json.Marshal(jsonData)
+
+	// POST 호출
+	resp, err = http.Post("http://52.78.185.234:7050/chaincode", "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
 		panic(err)
 	}
