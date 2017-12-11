18ee82b91fc6b7516be930efdf3dec1828c729b7
kminhy09 <kminhy09@gmail.com>
2017-12-12 00:41:25 +0900

    test

--- handlers/blockchain.go
+++ handlers/blockchain.go
@@ -50,12 +50,47 @@ func PostRegistrar(c echo.Context) error {
 	return c.JSON(http.StatusCreated, string(data))
 }
 
-func PostDeploy(c echo.Context) error {
-	params := map[string]string{
-		"enrollId":     "admin",
-		"enrollSecret": "Xurw3yU9zI0l",
+type (
+	Deploy struct {
+		Jsonrpc string `json:"jsonrpc"`
+		Method  string `json:"method"`
+		Params  Params `json:"params"`
+		ID      int    `json:"id"`
 	}
-	pbytes, _ := json.Marshal(params)
+
+	Params struct {
+		Type          int         `json:"type"`
+		ChaincodeID   ChaincodeID `json:"chaincodeID"`
+		CtorMsg       CtorMsg     `json:"ctorMsg"`
+		SecureContext string      `json:"secureContext"`
+	}
+
+	ChaincodeID struct {
+		Name string `json:"name"`
+	}
+	CtorMsg struct {
+		Args [5]string `json:"args"`
+	}
+)
+
+func PostDeploy(c echo.Context) error {
+
+	deploy := Deploy{}
+	deploy.Jsonrpc = "2.0"
+	deploy.Method = "deploy"
+	params := Params{}
+	params.Type = 1
+	chaincodeID := ChaincodeID{}
+	chaincodeID.Name = "mycc"
+	params.ChaincodeID = chaincodeID
+	ctorMsg := CtorMsg{}
+	ctorMsg.Args = [5]string{"init", "a", "100", "b", "200"}
+	params.CtorMsg = ctorMsg
+	params.SecureContext = "admin"
+	deploy.Params = params
+	deploy.ID = 1
+
+	pbytes, _ := json.Marshal(deploy)
 	buff := bytes.NewBuffer(pbytes)
 	resp, err := http.Post("http://52.78.185.234:7050/chaincode", "application/json", buff)
 	if err != nil {
